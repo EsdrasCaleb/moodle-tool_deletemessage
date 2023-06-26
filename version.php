cd87b8ff62b6d22200e172149f491e63aa7ef535
Esdras Caleb <esdrascaleb@gmail.com>
2023-06-25 21:24:48 -0300

    new version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // Plugin version.
-$plugin->version = 2023062115;
+$plugin->version = 2023062515;
 
 // Required Moodle version.
 $plugin->requires  = 2013050100; // Requires this Moodle version - at least 2.0.
