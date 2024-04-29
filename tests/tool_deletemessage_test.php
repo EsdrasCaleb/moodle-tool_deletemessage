1347a7d475d8ca1af1770ca30dc1755d836fc4a5
Esdras Caleb <esdrascaleb@gmail.com>
2024-04-29 16:18:03 -0300

    fix tests

--- tests/tool_deletemessage_test.php
+++ tests/tool_deletemessage_test.php
@@ -29,34 +29,84 @@ namespace tool_deletemessage;
  * Class test if this plugin is deleting things it should not delete
  * @author    Esdras Caleb
  * @copyright  2023 Esdras Caleb
+ * @package core_message
+ * @category test
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class tool_deletemessage_test extends \advanced_testcase {
 
     /**
+     * Test set up.
+     *
+     * This is executed before running any test in this file.
+     */
+    public function setUp(): void {
+        $this->preventResetByRollback(); // Messaging is not compatible with transactions.
+        $this->messagesink = $this->redirectMessages();
+        $this->resetAfterTest();
+    }
+    
+    /**
      * Make message to tests
      * @return int message id
      */
     private function make_message() {
+        global $DB;
+
         $userfrom = $this->getDataGenerator()->create_user();
         $userto = $this->getDataGenerator()->create_user();
 
         // Message text.
-        $messagetext = "hello";
+        $message = "hello";
 
-        // Create message object.
-        $message = new \core\message\message();
-        $message->component = 'core';
-        $message->name = 'instantmessage';
-        $message->userfrom = $userfrom;
-        $message->userto = $userto;
-        $message->subject = '';
-        $message->fullmessage = $messagetext;
-        $message->fullmessageformat = FORMAT_PLAIN;
-        $message->fullmessagehtml = $messagetext;
-        $message->smallmessage = $messagetext;
+        if (empty($time)) {
+            $time = time();
+        }
 
-        return message_send($message);
+        if ($notification) {
+            $record = new \stdClass();
+            $record->useridfrom = $userfrom->id;
+            $record->useridto = $userto->id;
+            $record->subject = 'No subject';
+            $record->fullmessage = $message;
+            $record->smallmessage = $message;
+            $record->timecreated = $time;
+
+            return $DB->insert_record('notifications', $record);
+        }
+
+        if ($userfrom->id == $userto->id) {
+            // It's a self conversation.
+            $conversation = \core_message\api::get_self_conversation($userfrom->id);
+            if (empty($conversation)) {
+                $conversation = \core_message\api::create_conversation(
+                    \core_message\api::MESSAGE_CONVERSATION_TYPE_SELF,
+                    [$userfrom->id]
+                );
+            }
+            $conversationid = $conversation->id;
+        } else if (!$conversationid = \core_message\api::get_conversation_between_users([$userfrom->id, $userto->id])) {
+            // It's an individual conversation between two different users.
+            $conversation = \core_message\api::create_conversation(
+                \core_message\api::MESSAGE_CONVERSATION_TYPE_INDIVIDUAL,
+                [
+                    $userfrom->id,
+                    $userto->id
+                ]
+            );
+            $conversationid = $conversation->id;
+        }
+
+        // Ok, send the message.
+        $record = new \stdClass();
+        $record->useridfrom = $userfrom->id;
+        $record->conversationid = $conversationid;
+        $record->subject = 'No subject';
+        $record->fullmessage = $message;
+        $record->smallmessage = $message;
+        $record->timecreated = $time;
+
+        return $DB->insert_record('messages', $record);
     }
 
     /**
