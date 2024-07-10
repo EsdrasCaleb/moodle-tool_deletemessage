a39347e23ddd84ef85817ca7aab0570ffa163f75
Esdras Caleb Oliveira Silva <esdrascaleb@gmail.com>
2024-07-10 07:09:00 -0300

    fix  issue id message not exists issue #4

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // Plugin version.
-$plugin->version = 2024051500;
+$plugin->version = 2024071000;
 
 // Required Moodle version.
 $plugin->requires  = 2018051718; // Requires this Moodle version - at least 3.5 (new messsage system).
